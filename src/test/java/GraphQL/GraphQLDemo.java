89a468530123f8d6564b4d951bd484e220debf4e
Nancy <er.nancybhardwaj@gmail.com>
2024-02-03 21:11:17 +0530

    First commit for Graph QL Sample API

---
+++ src/test/java/GraphQL/GraphQLDemo.java
@@ -0,0 +1,33 @@
+package GraphQL;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class GraphQLDemo {
+
+    @Test
+    public void testDummyAPI()
+    {
+        RestAssured.baseURI = "https://rickandmortyapi.com/graphql";
+
+      //  String query = "query character($id: ID!) { character(id: $id) { origin { id } location { id } created }}";
+
+       // String variables = "{\"id\":\"1\"}";
+
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body("{\"query\":\"query character($id: ID!) { character(id: $id) { origin { id } location { id } created }}\",\"variables\":{\"id\":\"1\"}}")
+                .when()
+                .post()
+                .then()
+                .extract()
+                .response();
+
+
+        response.prettyPrint();
+
+    }
+
+}
